9afdcc841d02d6a18e5a32a15c79c51eab514596
arunimaroy <arunima.roy23@gmail.com>
2022-10-16 12:37:45 +0530

    Updated framework

---
+++ src/main/java/utils/BaseTest.java
@@ -0,0 +1,91 @@
+package utils;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.markuputils.ExtentColor;
+import com.aventstack.extentreports.markuputils.Markup;
+import com.aventstack.extentreports.markuputils.MarkupHelper;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import com.aventstack.extentreports.reporter.configuration.Theme;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.ITestResult;
+import org.testng.annotations.*;
+
+import java.io.File;
+import java.lang.reflect.Method;
+import java.time.Duration;
+
+public class BaseTest {
+    public static WebDriver driver;
+    public ExtentSparkReporter htmlReporter;
+    public static ExtentReports extent;
+    public static ExtentTest logger;
+
+
+    @BeforeTest
+    public void beforeTestMethod()
+    {
+        htmlReporter = new ExtentSparkReporter(System.getProperty("user.dir")+File.separator+"reports"+File.separator+"AutomationReport.html");
+        htmlReporter.config().setEncoding("utf-8");
+        htmlReporter.config().setDocumentTitle("Automation Report");
+        htmlReporter.config().setReportName("Automation Test Results");
+        htmlReporter.config().setTheme(Theme.DARK);
+        extent=new ExtentReports();
+        extent.attachReporter(htmlReporter);
+        extent.setSystemInfo("Automation tester","Arunima Bose");
+    }
+
+
+    @BeforeMethod
+    @Parameters(value = {"browserName"})
+    public void beforeMethodMethod(String browserName, Method testMethod)
+    {
+        logger = extent.createTest(testMethod.getName());
+        setUpDriver(browserName);
+        driver.manage().window().maximize();
+        driver.get(Constants.url);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+    }
+
+
+    @AfterMethod
+    public void afterMethodMethod(ITestResult result)
+    {
+        if(result.getStatus()==ITestResult.SUCCESS) {
+            String methodName = result.getMethod().getMethodName();
+            String logText = "Test Case:" + methodName + "PASSED";
+            Markup m = MarkupHelper.createLabel(logText, ExtentColor.GREEN);
+            logger.log(Status.PASS,m);
+        } else if (result.getStatus()==ITestResult.FAILURE) {
+            String methodName = result.getMethod().getMethodName();
+            String logText = "Test Case:" + methodName + "FAILED";
+            Markup m = MarkupHelper.createLabel(logText, ExtentColor.RED);
+            logger.log(Status.FAIL,m);
+        }
+        driver.quit();
+    }
+
+
+    @AfterTest
+    public void afterTestMethod()
+    {
+        extent.flush();
+    }
+
+    public void setUpDriver(String browserName) {
+        if (browserName.equalsIgnoreCase("chrome")) {
+            System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + File.separator + "drivers" + File.separator + "chromedriver");
+            driver = new ChromeDriver();
+        } else if (browserName.equalsIgnoreCase("firefox")) {
+            System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir") + File.separator + "drivers" + File.separator + "geckodriver");
+            driver = new FirefoxDriver();
+        } else {
+            System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + File.separator + "drivers" + File.separator + "chromedriver");
+            driver = new ChromeDriver();
+        }
+    }
+}
